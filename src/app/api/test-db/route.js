9cebff10be92a5b4d0dd3796e327be0210db9e1e
rizqyazhar <rizqytask20@gmail.com>
2026-07-15 01:18:57 +0700

    clean endpoint test

--- src/app/api/test-db/route.js
+++
@@ -1,9 +0,0 @@
-import { sql } from "@/lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const tables = await sql`
-  SELECT table_name FROM information_schema.tables
-  WHERE table_schema = 'public'`;
-  return NextResponse.json({ tables });
-}
